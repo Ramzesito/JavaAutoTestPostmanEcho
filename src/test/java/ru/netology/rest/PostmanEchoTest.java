81af6b7b671fd6cec1337556e4fca42950855cb6
Ramzesito <roman_shv@mail.ru>
2023-07-30 22:19:42 +0300

    Init commit

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldReturnDataSent() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                //.get("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+
+    }
+}
